65806fb50f6a879b65a47805e15d950ac30c7a4d
Coursera Learner <nobody@nowhere.nul>
2018-06-18 00:56:24 +0000

    Did assignment 05_squares

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int max(int f1, int f2) {
+  if (f1 > f2) {
+    return f1;
+  }
+  else {
+    return f2;
+  }
+}
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
-  //compute the max of size1 and (x_offset + size2).  Call this w
+  int xsize = x_offset + size2;
+  int ysize = y_offset + size2;
+  int w = max(size1, xsize);
+  int h = max(size1, ysize);
+  for (int y = 0; y < h; y++){
+    for (int x = 0; x < w; x++){
+      if ((x >= x_offset && x < xsize) && (y == y_offset || y == ysize - 1)) {
+	printf("*");
+      } else if((y >= y_offset && y < ysize) && (x == x_offset || x == xsize - 1)) {
+	printf("*");
 
-  //compute the max of size1 and (y_offset + size2).  Call this h
-
-  //count from 0 to h. Call the number you count with y
-
-    //count from 0 to w. Call the number you count with x
-
-      //check if  EITHER
-      //    ((x is between x_offset  and x_offset +size2) AND 
-      //     y is equal to either y_offset OR y_offset + size2 - 1 )
-      //  OR
-      //    ((y is between y_offset and y_offset + size2) AND
-      //     x is equal to either x_offset OR x_offset + size2 -1)
-      // if so, print a *
-
-      //if not,
-      // check if EITHER
-      //    x is less than size1 AND (y is either 0 or size1-1)
-      // OR
-      //    y is less than size1 AND (x is either 0 or size1-1)
-      //if so, print a #
-
-      //else print a space
-    //when you finish counting x from 0 to w, 
-    //print a newline
-
+      } else {
+	if ((x < size1) && (y == 0 || y == size1 - 1)) {
+	  printf("#");
+	} else if((y < size1) && (x == 0 || x == size1 - 1)) {
+	  printf("#");
+	} else {
+	  printf(" ");
+	}
+      }
+    }
+    printf("\n");
+  }
 }
